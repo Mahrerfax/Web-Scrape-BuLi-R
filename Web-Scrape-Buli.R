ca0a0ceeef3d385767f47cec48a1ee4d0c8c04e0
Mahrerfax <117309388+Mahrerfax@users.noreply.github.com>
2023-04-10 14:32:00 +0200

    Update Web-Scrape-Buli.R

--- Web-Scrape-Buli.R
+++ Web-Scrape-Buli.R
@@ -2,6 +2,7 @@
 
 library(rvest)
 library(dplyr)
+library(esquisse)
 
 # Einlesen der Website
 
@@ -19,12 +20,18 @@ werte
 name <- website %>% html_nodes(".d-lg-block a") %>% html_text()
 name
 
+# Holt die Statistik-Bezeichnungen
+
+werte_name <- website %>% html_nodes(".title") %>% html_text()
+werte_name
+ 
 # Holt die restlichen statistiken die von .value nicht erfasst worden sind
 
 werte_rest <- website %>% html_nodes("span") %>% html_text()
 werte_rest
 
 # Zuweisung der einzelnen Variablen
+
 
 team_heim <- name[1]
 team_ausw <- name[2]
@@ -38,3 +45,57 @@ shots_heim <- shots_off_target+shots_on_target
 shots_off_target_ausw <- as.integer(werte[9])
 shots_on_target_ausw <- as.integer(werte[11])
 shots_ausw <- shots_off_target_ausw + shots_on_target_ausw
+paesse_heim <- as.integer(werte[13])
+paesse_ausw<- as.integer(werte[14])
+laufdistanz_heim <- as.double(werte[15])
+laufdistanz_ausw <- as.double(werte[16])
+sprints_heim <- as.integer(werte[17])
+sprints_ausw <- as.integer(werte[18])
+gewonnene_zweikämpfe_heim <- as.integer(werte[19])
+gewonnene_zweikämpfe_ausw<- as.integer(werte[20])
+ecken_heim <- as.integer(werte[21])
+ecken_ausw <- as.integer(werte[22])
+abseits_heim <- as.integer(werte[23])
+abseits_ausw <- as.integer(werte[24])
+fouls_heim <- as.integer(werte[25])
+fouls_ausw <- as.integer(werte[26])
+ballbesitz_heim <- as.integer(werte_rest[38])
+ballbesitz_ausw <- as.integer(werte_rest[39])
+werte_rest <- gsub("%", "", werte_rest)         #entfernen des "%" aus dem Wert um es anschließend in Integer umzuwandeln
+passquote_heim <- as.integer(werte_rest[43])
+passquote_ausw <- as.integer(werte_rest[45])
+
+
+# Dataframe erstellen
+
+spielstats <- data.frame( Team = c(team_heim,
+                                   team_ausw),
+                          Tore = c(goals_heim,
+                                   goals_ausw),
+                          xG = c(xgoals_heim,
+                                 xgoals_ausw),
+                          Torschuesse = c(shots_heim,
+                                          shots_ausw),
+                          Ballbeseitz = c(ballbesitz_heim,
+                                          ballbesitz_ausw),
+                          Pässe = c(paesse_heim,
+                                    paesse_ausw),
+                          Passquote = c(passquote_heim,
+                                        passquote_ausw),
+                          Ecken = c(ecken_heim,
+                                            ecken_ausw),
+                          Laufdistanz = c(laufdistanz_heim,
+                                          laufdistanz_ausw),
+                          Sprints = c(sprints_heim,
+                                      sprints_ausw),
+                          Zweikämpfe = c(gewonnene_zweikämpfe_heim,
+                                         gewonnene_zweikämpfe_ausw),
+                          Zweikampfquote = c((gewonnene_zweikämpfe_heim/(gewonnene_zweikämpfe_heim+gewonnene_zweikämpfe_ausw))*100,
+                                             (gewonnene_zweikämpfe_ausw/(gewonnene_zweikämpfe_heim+gewonnene_zweikämpfe_ausw))*100),
+                          Abseits = c(abseits_heim,
+                                      abseits_ausw),
+                          Fouls = c(fouls_heim,
+                                    fouls_ausw))
+
+
+
